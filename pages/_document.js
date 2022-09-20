95a584fb71f18b8da47afe903020e5a47db7768c
tharoonb <89992077+tharoonb@users.noreply.github.com>
2022-09-20 14:31:04 -0400

    title change

--- pages/_document.js
+++ pages/_document.js
@@ -10,11 +10,8 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <script async 
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9554987021243742"crossorigin="anonymous">
-
-          </script>
-          {/* <meta name="viewport" content="width=device-width,minimum-scale=1, initial-scale=1" /> */}
+          {<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9554987021243742"crossorigin="anonymous"></script>}
+          { <meta name="WOTDC" content="width=device-width,minimum-scale=1, initial-scale=1" /> }
         </Head>
         <body>
           <Main />
