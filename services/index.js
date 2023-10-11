fbb263e7233563c92c822fccf780df7b4e8b40e3
tharoonb <89992077+tharoonb@users.noreply.github.com>
2023-10-10 21:46:57 -0400

    publish fix

--- services/index.js
+++ services/index.js
@@ -5,7 +5,7 @@ const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 export const getPosts = async () => {
   const query = gql`
   query MyQuery {
-    postsConnection(orderBy: updatedAt_DESC) {
+    postsConnection(sort: publishedAt_DESC) {
       edges {
         node {
           author {
